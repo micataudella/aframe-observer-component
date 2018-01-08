ce7b5ca39ee1b1e588c34f08712bca4abd6e24b6
Michele Cataudella <michele.cataudella@guest.telecomitalia.it>
2018-01-08 09:05:40 +0100

    Updates

---
+++ dist/aframe-observer-component.js
@@ -0,0 +1,132 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+	/* global AFRAME */
+
+	if (typeof AFRAME === 'undefined') {
+	  throw new Error('Component attempted to register before AFRAME was available.');
+	}
+
+	/**
+	 * Observer component for A-Frame.
+	 */
+	AFRAME.registerComponent('observer', {
+	  schema: {
+	    // selector of the HTML element where the canvas with the camera view is going to be attached
+	    canvas: {
+	      type: 'string',
+	      default: ''
+	    },
+	    // desired FPS of observer display
+	    fps: {
+	      type: 'number',
+	      default: '30.0'
+	    }
+	  },
+
+	  /**
+	   * Set if component needs multiple instancing.
+	   */
+	  multiple: false,
+
+	  /**
+	   * Called once when component is attached. Generally for initial setup.
+	   */
+	  init: function () {
+	    var targetEl = document.querySelector(this.data.canvas);
+	    this.counter = 0;
+	    this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+	    this.renderer.setPixelRatio( window.devicePixelRatio );
+	    this.renderer.setSize( targetEl.offsetWidth, targetEl.offsetHeight );
+	    // creates canvas
+	    targetEl.appendChild(this.renderer.domElement)
+	  },
+
+	  /**
+	   * Called when component is attached and when component data changes.
+	   * Generally modifies the entity based on the data.
+	   */
+	  update: function (oldData) { },
+
+	  /**
+	   * Called when a component is removed (e.g., via removeAttribute).
+	   * Generally undoes all modifications to the entity.
+	   */
+	  remove: function () { },
+
+	  /**
+	   * Called on each scene tick.
+	   */
+	  tick: function (t, delta) {
+	    var loopFPS = 1000.0 / delta;
+	    var hmdIsXFasterThanDesiredFPS = loopFPS / this.data.fps;
+	    var renderEveryNthFrame = Math.round(hmdIsXFasterThanDesiredFPS);
+	    if (this.counter % renderEveryNthFrame === 0) {
+	      this.render(delta);
+	    }
+	    this.counter += 1;
+	  },
+
+	  render: function() {
+	    this.renderer.render( this.el.sceneEl.object3D , this.el.object3DMap.camera );
+	  },
+
+	  /**
+	   * Called when entity pauses.
+	   * Use to stop or remove any dynamic or background behavior such as events.
+	   */
+	  pause: function () { },
+
+	  /**
+	   * Called when entity resumes.
+	   * Use to continue or add any dynamic or background behavior such as events.
+	   */
+	  play: function () { }
+	});
+
+
+/***/ })
+/******/ ]);
